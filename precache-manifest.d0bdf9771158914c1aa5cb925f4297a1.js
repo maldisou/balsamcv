4c74f9ab24a9301385c01fe9286ac643e229ad94
maldisou42 <maldisou@student.42abudhabi.ae>
2023-01-02 17:01:16 +0400

    Updates

---
+++ precache-manifest.d0bdf9771158914c1aa5cb925f4297a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3eee3a9ff0f2e322df9",
+    "url": "/balsamcv/static/css/main.7c7a9c28.chunk.css"
+  },
+  {
+    "revision": "c3eee3a9ff0f2e322df9",
+    "url": "/balsamcv/static/js/main.9de36fcd.chunk.js"
+  },
+  {
+    "revision": "3942fbc87dbf8644493f",
+    "url": "/balsamcv/static/js/runtime~main.cf8fba23.js"
+  },
+  {
+    "revision": "59c5f6978674a4eb4499",
+    "url": "/balsamcv/static/js/2.348d2c5c.chunk.js"
+  },
+  {
+    "revision": "6c40851f38de19fc8c13",
+    "url": "/balsamcv/static/js/3.77588386.chunk.js"
+  },
+  {
+    "revision": "76a491e1f3e660987c2d22e4d0353d26",
+    "url": "/balsamcv/index.html"
+  }
+];
